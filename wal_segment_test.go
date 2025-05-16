4e8ff2573744bcc7eed8367ba6fe63568c866489
halfbreak <14543067+halfbreak@users.noreply.github.com>
2025-05-16 14:25:08 +0100

    add unit tests

--- wal_segment_test.go
+++ wal_segment_test.go
@@ -29,7 +29,7 @@ func TestNewWalSegment(t *testing.T) {
 
 func TestAppend(t *testing.T) {
 	tmp := setupTempFolder(t)
-	segment, err := NewWalSegment(tmp, 2)
+	segment, err := NewWalSegment(tmp, 0)
 	assert.NoError(t, err)
 	defer segment.Close()
 
@@ -39,4 +39,33 @@ func TestAppend(t *testing.T) {
 
 	actual := (*segment.mmappedData)[:len(data)]
 	assert.Equal(t, string(data), string(actual))
+}
+
+func TestIsFull(t *testing.T) {
+	tmp := setupTempFolder(t)
+	segment, err := NewWalSegment(tmp, 0)
+	assert.NoError(t, err)
+	defer segment.Close()
+
+	segment.offset = mmapSize - 5
+
+	data := []byte("123456") // 6 bytes
+	assert.Equal(t, true, segment.IsFull(data))
+
+	data = []byte("1234") // 4 bytes
+	assert.Equal(t, false, segment.IsFull(data))
+}
+
+func TestFlushAndClose(t *testing.T) {
+	tmp := setupTempFolder(t)
+	segment, err := NewWalSegment(tmp, 0)
+	assert.NoError(t, err)
+
+	segment.Append([]byte("flushed content"))
+
+	err = segment.Flush()
+	assert.NoError(t, err)
+
+	err = segment.Close()
+	assert.NoError(t, err)
 }
