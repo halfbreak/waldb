b103052fcbefe50bc275c969c0ebdebd29a96824
halfbreak <14543067+halfbreak@users.noreply.github.com>
2025-05-16 15:19:30 +0100

    add checksum

--- wal_entry.go
+++ wal_entry.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"errors"
+	"hash/crc32"
 )
 
 type WALEntry struct {
@@ -17,7 +18,8 @@ func (e *WALEntry) Encode() []byte {
 	keyLen := uint32(len(keyBytes))
 	valLen := uint32(len(e.Value))
 
-	buf := make([]byte, 8+4+len(keyBytes)+4+len(e.Value)) // timestamp + keyLen + key + valLen + val
+	bodyLen := 8 + 4 + len(keyBytes) + 4 + len(e.Value) // timestamp + keyLen + key + valLen + val
+	buf := make([]byte, bodyLen+4)                      // + CRC
 
 	binary.BigEndian.PutUint64(buf[0:], uint64(e.Timestamp))
 	binary.BigEndian.PutUint32(buf[8:], keyLen)
@@ -25,12 +27,24 @@ func (e *WALEntry) Encode() []byte {
 	binary.BigEndian.PutUint32(buf[12+keyLen:], valLen)
 	copy(buf[16+keyLen:], e.Value)
 
+	crc := crc32.ChecksumIEEE(buf[:bodyLen])
+	binary.BigEndian.PutUint32(buf[bodyLen:], crc)
+
 	return buf
 }
 
 func Decode(data []byte) (*WALEntry, error) {
-	if len(data) < 16 {
+	if len(data) < 20 {
 		return nil, errors.New("data too short to contain header")
+	}
+
+	bodyLen := len(data) - 4
+	expectedCRC := binary.BigEndian.Uint32(data[bodyLen:])
+
+	// Verify checksum
+	actualCRC := crc32.ChecksumIEEE(data[:bodyLen])
+	if actualCRC != expectedCRC {
+		return nil, errors.New("checksum mismatch")
 	}
 
 	ts := int64(binary.BigEndian.Uint64(data[0:8]))
